24f10725bac03e7ca61e909444eff94a3b3b003d
hasanasif96 <45166409+hasanasif96@users.noreply.github.com>
2021-04-17 17:15:04 +0530

    Update settings.py

--- ftpmart/settings.py
+++ ftpmart/settings.py
@@ -118,9 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS=[BASE_DIR / "static"]
-STATIC_ROOT= BASE_DIR / "static_cdb"
+#STATIC_URL = '/static/'
+#STATICFILES_DIRS=[BASE_DIR / "static"]
+#STATIC_ROOT= BASE_DIR / "static_cdb"
 MEDIA_URL="/media/"
 MEDIA_ROOT= BASE_DIR / "media"
 
