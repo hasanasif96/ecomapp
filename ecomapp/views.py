a9ab7612b20aa0e1050944dc6512e0331b8b71ab
hasanasif96 <45166409+hasanasif96@users.noreply.github.com>
2021-04-19 13:11:50 +0530

    Update views.py

--- ecomapp/views.py
+++ ecomapp/views.py
@@ -121,14 +121,6 @@ class MyCartView(cartnum,EcomMixin, TemplateView):
     template_name = "mycart.html"
     
     
-    def dispatch(self, request, *args, **kwargs):
-        if request.user.is_authenticated and request.user.customer:
-            pass
-        else:
-            return redirect("/login/?next=/my-cart/")
-        return super().dispatch(request, *args, **kwargs)
-    
-    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         cart_id = self.request.session.get("cart_id", None)
