54e148fc3f7b6d03c25f270bc366b8b4fc5acd4f
hasanasif96 <45166409+hasanasif96@users.noreply.github.com>
2021-04-21 15:04:40 +0530

    Update settings.py

--- ftpmart/settings.py
+++ ftpmart/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'bh38y214ee3rh8k11re^-i%23)2%q#(^m0kc%&2^h2=m5=^xv('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["citymart.herokuapp.com",]
 
