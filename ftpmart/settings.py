c57668f8c38b6dfb37d404bd84f26b931c8b8168
hasanasif96 <45166409+hasanasif96@users.noreply.github.com>
2021-04-21 15:01:45 +0530

    Update settings.py

--- ftpmart/settings.py
+++ ftpmart/settings.py
@@ -133,5 +133,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "hasif5158@gmail.com"
-EMAIL_HOST_PASSWORD = "Mrlonely@56"
+EMAIL_HOST_PASSWORD = "Corona@123"
 
