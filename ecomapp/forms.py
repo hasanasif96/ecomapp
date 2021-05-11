e23daa87c72c01bd17af206b242125a78b94eac7
hasanasif96 <45166409+hasanasif96@users.noreply.github.com>
2021-05-11 13:09:37 +0530

    Update forms.py

--- ecomapp/forms.py
+++ ecomapp/forms.py
@@ -16,13 +16,17 @@ class CheckoutForm(forms.ModelForm):
                 "email": forms.TextInput(attrs={'class':'form-control'})}
         
 class CustomerRegistrationForm(forms.ModelForm):
-    username = forms.CharField(widget=forms.TextInput())
-    password = forms.CharField(widget=forms.PasswordInput())
-    email = forms.CharField(widget=forms.EmailInput())
+    username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    email = forms.CharField(widget=forms.EmailInput(attrs={'class':'form-control'}))
+    full_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    address = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
 
     class Meta:
         model = Customer
         fields = ["username", "password", "email", "full_name", "address"]
+        
+   
 
     def clean_username(self):
         uname = self.cleaned_data.get("username")
@@ -33,8 +37,8 @@ class CustomerRegistrationForm(forms.ModelForm):
 
 
 class CustomerLoginForm(forms.Form):
-    username = forms.CharField(widget=forms.TextInput())
-    password = forms.CharField(widget=forms.PasswordInput())
+    username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
 
 
 
